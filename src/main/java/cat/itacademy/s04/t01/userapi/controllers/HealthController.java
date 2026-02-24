61f53467131a2822f6290dec9456249fa1ae3a59
Javier Castillo <javiercame98@gmail.com>
2026-02-24 19:57:37 +0100

    feat: add HealthStatus record for JSON response

--- src/main/java/cat/itacademy/s04/t01/userapi/controllers/HealthController.java
+++ src/main/java/cat/itacademy/s04/t01/userapi/controllers/HealthController.java
@@ -1,5 +1,6 @@
 package cat.itacademy.s04.t01.userapi.controllers;
 
+import cat.itacademy.s04.t01.userapi.model.HealthStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,8 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthController {
 
     @GetMapping("/health")
-    public String checkOk(){
-        return "ok";
+    public HealthStatus checkOk(){
+        return new HealthStatus("Ok");
     }
 
 }
