e39bfcd058aab02dd2338102807bfe124aaaca4f
Javier Castillo <javiercame98@gmail.com>
2026-02-26 18:31:33 +0100

    feat: add POST /users endpoint using UserDTO to create new users

--- src/main/java/cat/itacademy/s04/t01/userapi/controllers/UserController.java
+++ src/main/java/cat/itacademy/s04/t01/userapi/controllers/UserController.java
@@ -1,11 +1,15 @@
 package cat.itacademy.s04.t01.userapi.controllers;
 
+import cat.itacademy.s04.t01.userapi.dto.UserDTO;
 import cat.itacademy.s04.t01.userapi.model.User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 public class UserController {
@@ -17,4 +21,13 @@ public class UserController {
     private List<User> getAllUsers(){
         return userList;
     }
+
+    @PostMapping("/users")
+    public User addUser (@RequestBody UserDTO userDTO){
+        UUID id = UUID.randomUUID();
+        User newUser = new User(id, userDTO.name(), userDTO.email());
+        userList.add(newUser);
+        return newUser;
+    }
+
 }
