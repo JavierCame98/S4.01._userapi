628aaad8e19b2a6844a325505708fa8c4f77b86f
Javier Castillo <javiercame98@gmail.com>
2026-02-26 18:51:42 +0100

    feat: add GET /users/{id} endpoint with custom 404 exception

--- src/main/java/cat/itacademy/s04/t01/userapi/controllers/UserController.java
+++ src/main/java/cat/itacademy/s04/t01/userapi/controllers/UserController.java
@@ -1,11 +1,9 @@
 package cat.itacademy.s04.t01.userapi.controllers;
 
 import cat.itacademy.s04.t01.userapi.dto.UserDTO;
+import cat.itacademy.s04.t01.userapi.exceptions.UserNotFoundException;
 import cat.itacademy.s04.t01.userapi.model.User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,7 +16,7 @@ public class UserController {
 
 
     @GetMapping("/users")
-    private List<User> getAllUsers(){
+    public List<User> getAllUsers(){
         return userList;
     }
 
@@ -30,4 +28,11 @@ public class UserController {
         return newUser;
     }
 
+    @GetMapping("/users/{id}")
+    public User getUserById(@PathVariable UUID id){
+        return userList.stream()
+                .filter(user -> user.id().equals(id))
+                .findFirst()
+                .orElseThrow( () -> new UserNotFoundException("User not found by id: " + id));
+    }
 }
