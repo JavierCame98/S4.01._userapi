4e5b8a3af6778941d8250657658f3b66692864e6
Javier Castillo <javiercame98@gmail.com>
2026-02-26 17:48:14 +0100

    feat: creating UserController with a userList Array as a mock database

---
+++ src/main/java/cat/itacademy/s04/t01/userapi/controllers/UserController.java
@@ -0,0 +1,15 @@
+package cat.itacademy.s04.t01.userapi.controllers;
+
+import cat.itacademy.s04.t01.userapi.model.User;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    private List<User> userList = new ArrayList<>();
+
+
+}
