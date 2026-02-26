f96161ee4199ee2f058e2089debf05cc5e0d1950
Javier Castillo <javiercame98@gmail.com>
2026-02-26 17:50:22 +0100

    add GET /users endpoint to list all users

--- src/main/java/cat/itacademy/s04/t01/userapi/controllers/UserController.java
+++ src/main/java/cat/itacademy/s04/t01/userapi/controllers/UserController.java
@@ -1,6 +1,7 @@
 package cat.itacademy.s04.t01.userapi.controllers;
 
 import cat.itacademy.s04.t01.userapi.model.User;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -12,4 +13,8 @@ public class UserController {
     private List<User> userList = new ArrayList<>();
 
 
+    @GetMapping
+    private List<User> getAllUsers(){
+        return userList;
+    }
 }
