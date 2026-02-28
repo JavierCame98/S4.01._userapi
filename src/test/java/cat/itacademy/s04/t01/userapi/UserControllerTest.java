a669e0795ccaa339c247dcb3402d4881b0abb19f
Javier Castillo <javiercame98@gmail.com>
2026-02-28 12:11:41 +0100

    test: creating getUsers_returnsEmptyListInitially test

--- src/test/java/cat/itacademy/s04/t01/userapi/UserControllerTest.java
+++ src/test/java/cat/itacademy/s04/t01/userapi/UserControllerTest.java
@@ -2,10 +2,56 @@ package cat.itacademy.s04.t01.userapi;
 
 
 import cat.itacademy.s04.t01.userapi.controllers.UserController;
+import org.junit.jupiter.api.MediaType;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import tools.jackson.databind.ObjectMapper;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @WebMvcTest(UserController.class)
 public class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void getUsers_returnsEmptyListInitially() throws Exception {
+        mockMvc.perform(get("/users"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$").isEmpty());
+    }
+
+    @Test
+    void createUser_returnsUserWithId() {
+        // Simula POST /users amb JSON
+        // Espera que torni el mateix usuari amb UUID no nul
+    }
+
+    @Test
+    void getUserById_returnsCorrectUser() {
+        // Primer afegeix un usuari amb POST
+        // Després GET /users/{id} i comprova que torni aquest usuari
+    }
+
+    @Test
+    void getUserById_returnsNotFoundIfMissing() {
+        // Simula GET /users/{id} amb un id aleatori
+        // Espera 404
+    }
+
+    @Test
+    void getUsers_withNameParam_returnsFilteredUsers() {
+        // Afegeix dos usuaris amb POST
+        // Fa GET /users?name=jo i comprova que només torni els que contenen "jo"
+    }
 
 
 }
